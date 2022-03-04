a6f6eaed0fe54335a1286114bafe0b943e33bdf1
p22strautz <p22strautz@fairfieldprep.org>
2022-03-04 10:10:14 -0500

    lab 13-1, 13-2

---
+++ lab_13-2.py
@@ -0,0 +1,15 @@
+# Author: SCT (AMDG) 3/3/22
+
+def build_house(p1, p2, p3, p4):
+    color = p1
+    windows = p2
+    doors = p3
+    bathrooms = p4
+    print("Your house is {0}, has {1} windows, {2} doors, and {3} bathrooms.".format(color, windows, doors, bathrooms))
+
+q1 = input("Enter the color of your house.\n")
+q2 = input("Enter the number of windows your house has.\n")
+q3 = input("Enter the number of doors your house has.\n")
+q4 = input("Enter the number of bathrooms your house has.\n")
+
+print(build_house(q1, q2, q3, q4))
